63c11e6ad0be198bf50ec68cf2ce5e4a2d4d7801
eshika <eshika22som2003@gmail.com>
2022-10-14 18:40:57 +0530

    besh

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 const Button = () => {
   return (
+    <div className='btn'>
     <div className='button'>
        <span className='register'> R E G I S T E R </span>
        <span> N O W</span>
+    </div>
     </div>
   )
 }
